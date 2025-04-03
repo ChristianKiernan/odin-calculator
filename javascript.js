b52bd188a441ec8c1b02bea6e97381a8e80ac39a
Christian Kiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-04-03 14:03:03 -0400

    fix: extended functionality to all number buttons and operation buttons (+, -, *, /)

--- javascript.js
+++ javascript.js
@@ -54,6 +54,14 @@ const buttonFour = document.querySelector("#button-4");
 buttonFour.innerHTML = "/";
 buttonFour.classList.add("calc-button-blue");
 
+buttonFour.addEventListener("click", () => {
+    if (displayIsEmpty() === false) {
+        currentInput = false;
+        operator = "/";
+        expressionComplete = true;
+    }
+});
+
 //ButtonFive (7)
 const buttonFive = document.querySelector("#button-5");
 buttonFive.innerHTML = "7";
@@ -116,39 +124,230 @@ buttonSix.addEventListener("click", () => {
 const buttonSeven = document.querySelector("#button-7");
 buttonSeven.innerHTML = "9";
 
+buttonSeven.addEventListener("click", () => {
+    if (displayIsEmpty() && expressionComplete === false) {
+        firstNum = 9;
+        currentInput = true;
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === false) {
+        let stringNum = String(firstNum);
+        let newStringNum = stringNum + "9";
+        firstNum = parseInt(newStringNum);
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() && expressionComplete === true) {
+        secondNum = 9;
+        currentInput = true;
+        display.innerHTML = `${secondNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === true) {
+        let stringNum = secondNum.toString();
+        let newStringNum = stringNum + "9";
+        secondNum = parseInt(newStringNum);
+        display.innerHTML = `${secondNum}`
+    }
+});
+
 //ButtonEight (*)
 const buttonEight = document.querySelector("#button-8");
 buttonEight.innerHTML = "*";
 buttonEight.classList.add("calc-button-blue");
 
+buttonEight.addEventListener("click", () => {
+    if (displayIsEmpty() === false) {
+        currentInput = false;
+        operator = "*";
+        expressionComplete = true;
+    }
+});
+
 //ButtonNine (4)
 const buttonNine= document.querySelector("#button-9");
 buttonNine.innerHTML = "4";
+
+buttonNine.addEventListener("click", () => {
+    if (displayIsEmpty() && expressionComplete === false) {
+        firstNum = 4;
+        currentInput = true;
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === false) {
+        let stringNum = String(firstNum);
+        let newStringNum = stringNum + "4";
+        firstNum = parseInt(newStringNum);
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() && expressionComplete === true) {
+        secondNum = 4;
+        currentInput = true;
+        display.innerHTML = `${secondNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === true) {
+        let stringNum = secondNum.toString();
+        let newStringNum = stringNum + "4";
+        secondNum = parseInt(newStringNum);
+        display.innerHTML = `${secondNum}`
+    }
+});
 
 //ButtonTen (5)
 const buttonTen = document.querySelector("#button-10");
 buttonTen.innerHTML = "5";
 
+buttonTen.addEventListener("click", () => {
+    if (displayIsEmpty() && expressionComplete === false) {
+        firstNum = 5;
+        currentInput = true;
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === false) {
+        let stringNum = String(firstNum);
+        let newStringNum = stringNum + "5";
+        firstNum = parseInt(newStringNum);
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() && expressionComplete === true) {
+        secondNum = 5;
+        currentInput = true;
+        display.innerHTML = `${secondNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === true) {
+        let stringNum = secondNum.toString();
+        let newStringNum = stringNum + "5";
+        secondNum = parseInt(newStringNum);
+        display.innerHTML = `${secondNum}`
+    }
+});
+
 //ButtonEleven (6)
 const buttonEleven= document.querySelector("#button-11");
 buttonEleven.innerHTML = "6";
+
+buttonEleven.addEventListener("click", () => {
+    if (displayIsEmpty() && expressionComplete === false) {
+        firstNum = 6;
+        currentInput = true;
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === false) {
+        let stringNum = String(firstNum);
+        let newStringNum = stringNum + "6";
+        firstNum = parseInt(newStringNum);
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() && expressionComplete === true) {
+        secondNum = 6;
+        currentInput = true;
+        display.innerHTML = `${secondNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === true) {
+        let stringNum = secondNum.toString();
+        let newStringNum = stringNum + "6";
+        secondNum = parseInt(newStringNum);
+        display.innerHTML = `${secondNum}`
+    }
+});
 
 //ButtonTwelve (-)
 const buttonTwelve = document.querySelector("#button-12");
 buttonTwelve.innerHTML = "-";
 buttonTwelve.classList.add("calc-button-blue");
 
+buttonTwelve.addEventListener("click", () => {
+    if (displayIsEmpty() === false) {
+        currentInput = false;
+        operator = "-";
+        expressionComplete = true;
+    }
+});
+
 //ButtonThirteen (1)
 const buttonThirteen = document.querySelector("#button-13");
 buttonThirteen.innerHTML = "1";
+
+buttonThirteen.addEventListener("click", () => {
+    if (displayIsEmpty() && expressionComplete === false) {
+        firstNum = 1;
+        currentInput = true;
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === false) {
+        let stringNum = String(firstNum);
+        let newStringNum = stringNum + "1";
+        firstNum = parseInt(newStringNum);
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() && expressionComplete === true) {
+        secondNum = 1;
+        currentInput = true;
+        display.innerHTML = `${secondNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === true) {
+        let stringNum = secondNum.toString();
+        let newStringNum = stringNum + "1";
+        secondNum = parseInt(newStringNum);
+        display.innerHTML = `${secondNum}`
+    }
+});
 
 //ButtonFourteen (2)
 const buttonFourteen = document.querySelector("#button-14");
 buttonFourteen.innerHTML = "2";
 
+buttonFourteen.addEventListener("click", () => {
+    if (displayIsEmpty() && expressionComplete === false) {
+        firstNum = 2;
+        currentInput = true;
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === false) {
+        let stringNum = String(firstNum);
+        let newStringNum = stringNum + "2";
+        firstNum = parseInt(newStringNum);
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() && expressionComplete === true) {
+        secondNum = 2;
+        currentInput = true;
+        display.innerHTML = `${secondNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === true) {
+        let stringNum = secondNum.toString();
+        let newStringNum = stringNum + "2";
+        secondNum = parseInt(newStringNum);
+        display.innerHTML = `${secondNum}`
+    }
+});
+
 //ButtonFifteen (3)
 const buttonFifteen= document.querySelector("#button-15");
 buttonFifteen.innerHTML = "3";
+
+buttonFifteen.addEventListener("click", () => {
+    if (displayIsEmpty() && expressionComplete === false) {
+        firstNum = 3;
+        currentInput = true;
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === false) {
+        let stringNum = String(firstNum);
+        let newStringNum = stringNum + "3";
+        firstNum = parseInt(newStringNum);
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() && expressionComplete === true) {
+        secondNum = 3;
+        currentInput = true;
+        display.innerHTML = `${secondNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === true) {
+        let stringNum = secondNum.toString();
+        let newStringNum = stringNum + "3";
+        secondNum = parseInt(newStringNum);
+        display.innerHTML = `${secondNum}`
+    }
+});
 
 //ButtonSixteen (+)
 const buttonSixteen = document.querySelector("#button-16");
@@ -166,21 +365,69 @@ buttonSixteen.addEventListener("click", () => {
     }
 });
 
-
 //ButtonSeventeen (0)
 const buttonSeventeen = document.querySelector("#button-17");
 buttonSeventeen.innerHTML = "0";
+
+buttonSeventeen.addEventListener("click", () => {
+    if (displayIsEmpty() && expressionComplete === false) {
+        firstNum = 0;
+        currentInput = true;
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === false) {
+        let stringNum = String(firstNum);
+        let newStringNum = stringNum + "0";
+        firstNum = parseInt(newStringNum);
+        display.innerHTML = `${firstNum}`;
+    }
+    else if (displayIsEmpty() && expressionComplete === true) {
+        secondNum = 0;
+        currentInput = true;
+        display.innerHTML = `${secondNum}`;
+    }
+    else if (displayIsEmpty() === false && expressionComplete === true) {
+        let stringNum = secondNum.toString();
+        let newStringNum = stringNum + "0";
+        secondNum = parseInt(newStringNum);
+        display.innerHTML = `${secondNum}`
+    }
+});
 
 //ButtonEighteen (=)
 const buttonEighteen= document.querySelector("#button-18");
 buttonEighteen.innerHTML = "=";
 
 buttonEighteen.addEventListener("click", () => {
+
     //Calls correct function based on operand, and resets currentInput and expressionComplete so as to terminate the current expression
     if (operator === '+') {
         display.innerHTML = `${add(firstNum, secondNum)}`;
         currentInput = false;
         expressionComplete = false;
+        firstNum = 0;
+        secondNum = 0;
+    }
+    else if (operator === '-') {
+        display.innerHTML = `${subtract(firstNum, secondNum)}`;
+        currentInput = false;
+        expressionComplete = false;
+        firstNum = 0;
+        secondNum = 0;
+    }
+    else if (operator === '*') {
+        display.innerHTML = `${multiply(firstNum, secondNum)}`;
+        currentInput = false;
+        expressionComplete = false;
+        firstNum = 0;
+        secondNum = 0;
+    }
+    else if (operator === '/') {
+        display.innerHTML = `${divide(firstNum, secondNum)}`;
+        currentInput = false;
+        expressionComplete = false;
+        firstNum = 0;
+        secondNum = 0;
     }
 });
 
